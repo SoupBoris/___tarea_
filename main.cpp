69eb30f8939b7acf29a5f61307a72ce530ee52b8
soupboris <sebastian.berendson@utec.edu.pe>
2020-10-04 23:48:09 -0500

    lasttry

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-//#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "my_vector.h"
 
 using namespace std;
 
 
-int main(){
+/*int main(){
 utec::my_vector<int> vec3a;
     utec::my_vector<int> vec3b;
     utec::my_vector<int> vec3c;
@@ -16,3 +16,4 @@ utec::my_vector<int> vec3a;
     for (size_t i = 0; i < vec3c.size(); ++i)
         total += vec3c[i];
     }
+*/
